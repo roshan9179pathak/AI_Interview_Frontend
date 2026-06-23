d6e8aa9919ed8435801108377abe512f94cf6ef5
Roshan Pathak <roshanforplacements@gmail.com>
2026-06-23 23:03:42 +0530

    Changed backend url

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://ai-interview-backend-xpu5.onrender.com"
+export const ServerUrl  = "https://ai-interview-backend-1-ogaa.onrender.com"
 
 function App() {
 
